1b672470f37bb19e0c4d7fd048d944b01e4cbd94
Fouad Chmainy <fchmainy@yahoo.com>
2022-10-07 15:10:58 +0200

    Create variables.tf

---
+++ multiple_waf_policies/variables.tf
@@ -0,0 +1,24 @@
+variable name {
+        type    = string
+        default = ""
+
+}
+
+variable partition {
+        type    = string
+        default = "Common"
+}
+
+variable rules {
+        type = list(object({
+                name            = string
+                hostname        = optional(list(string))
+                path            = optional(list(string))
+                policy          = string
+        }))
+}
+
+variable default_policy {
+	    type	= string
+	    default	= ""
+}
